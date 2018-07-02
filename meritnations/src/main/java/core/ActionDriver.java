ce74a3e35e2d1acb94191e563b94b034985dfaa5
Ankush <ankus.sam@gmail.com>
2018-07-02 22:26:39 +0530

    Created ActionDriver class for keywords

---
+++ meritnations/src/main/java/core/ActionDriver.java
@@ -0,0 +1,34 @@
+package core;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ActionDriver {
+	protected WebDriver driver;
+	protected WebDriverWait wait;
+	private long shortTime = 30;
+	public ActionDriver(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	protected void waitForElement(By locator) {
+		wait = new WebDriverWait(driver, shortTime);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+	}
+	
+	protected void type(By locator, String text) {
+		waitForElement(locator);
+		WebElement element = driver.findElement(locator);
+		element.sendKeys(text);
+	}
+	
+	protected void click(By locator) {
+		waitForElement(locator);
+		WebElement element = driver.findElement(locator);
+		element.click();
+	}
+
+}
